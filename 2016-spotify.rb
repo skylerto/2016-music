3053a7a2d968379a1c53f261b73ce76f58f0cd66
skylerl <skylerl@indellient.com>
2017-01-03 09:39:00 -0500

    upgrade to better ruby

--- 2016-spotify.rb
+++ 2016-spotify.rb
@@ -1,19 +1,45 @@
 require 'json'
 
-data = ''
-file = File.new('2016-spotify.json', 'r')
-while (line = file.gets)
-	data = "#{data}\n#{line}"
+module Spotify
+  class Data
+    attr_accessor :data, :names
+
+    def initialize(filename)
+      @filename = filename
+      @data = self.read_file
+      @names = self.fetch_names
+    end
+
+    def read_file
+      file_contents = ''
+      file = File.new(@filename, 'r')
+      while (line = file.gets)
+	file_contents = "#{file_contents}\n#{line}"
+      end
+      file.close
+      file_contents
+    end
+
+    def fetch_names
+      if @data
+        names = []
+        jason = JSON.parse(data)
+        jason['items'].each{ |item|
+          names << item['name']
+        }
+      else
+        @data = self.read_file
+        self.fetch_names
+      end
+      names
+    end
+
+    def artist_chart
+      chart = ''
+      @names.each_with_index { |name, i|
+        chart = "#{chart}#{i + 1}. #{name}\n"
+      }
+      chart
+    end
+  end
 end
-file.close
-# puts data
-
-names = []
-jason = JSON.parse(data)
-jason['items'].each{ |item|
-	names << item['name']
-}
-
-names.each_with_index { |name, i|
-	puts "#{i + 1}. #{name}"
-}
